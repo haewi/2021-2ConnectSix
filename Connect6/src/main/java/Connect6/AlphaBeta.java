65eb32b0f86463cfbbd6fc6e485dc1b7ac001bee
Yeahn <21900151@handong.edu>
2021-10-30 19:38:44 +0900

    kim

--- Connect6/src/main/java/Connect6/AlphaBeta.java
+++ Connect6/src/main/java/Connect6/AlphaBeta.java
@@ -1,7 +1,40 @@
 package Connect6;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 public class AlphaBeta {
-	public static int minMax(Board board, int depth, int alpha, int beta, boolean maximizingPlayer) { // kim
-		return 0;
+	public static int miniMax(Board board, Point position, int depth, int alpha, int beta, boolean maximizingPlayer) { // kim
+		if(depth == 0) {
+			return static evaluation of position
+		}
+		
+		board.updateBoard(position.x, position.y);
+		ArrayList<Point> children = board.getChild(board);
+		
+		if(maximizingPlayer) {
+			int maxEval = Integer.MIN_VALUE;
+			for(Point child : children) {
+				int eval = miniMax(board, child, depth-1, alpha, beta, false);
+				maxEval = Integer.max(alpha, eval);
+				alpha = Integer.max(alpha, eval);
+				if(beta <= alpha) {
+					break;
+				}
+			}
+			return maxEval;
+		}
+		else {
+			int minEval = Integer.MAX_VALUE;
+			for(Point child : children) {
+				int eval = miniMax(board, child, depth-1, alpha, beta, true);
+				minEval = Integer.min(minEval, eval);
+				beta = Integer.min(beta, eval);
+				if(beta <= alpha) {
+					break;
+				}
+				return minEval;
+			}
+		}
 	}
 }
--- Connect6/src/main/java/Connect6/Board.java
+++ Connect6/src/main/java/Connect6/Board.java
@@ -1,27 +1,42 @@
 package Connect6;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 public class Board {
 	final static int EMPTY = 0;
 	final static int RED = 1;
 	final static int BLACK = 2;
 	final static int WHITE = 3;
+	final static int SPACENUM = 19;
 	
-	int[][] board = new int[19][19]; // [세로][가로]
-	int[][] weight = new int[19][19];
+	int[][] board = new int[SPACENUM][SPACENUM]; // [세로][가로]
+	int[][] weight = new int[SPACENUM][SPACENUM];
 	
 	void Board() {
-		for(int i=0; i<19; i++) {
-			for(int j=0; j<19; j++) {
+		for(int i=0; i<SPACENUM; i++) {
+			for(int j=0; j<SPACENUM; j++) {
 				board[i][j] = EMPTY;
 			}
 		}
 	}
 	
 	public void updateBoard(int x, int y) {} // seo
-	public int askBoard(int x, int y) {} // kim
-	public ArrayList<Point> getChild(Board board) {} // seo
-	public void calculateWeight() {} // 가로 세로 우대각선 좌대각선 전부 계산 시키는 함수
-	public int getScore(int start, int direct) {} // 6칸 짜리 값 업데이트 // start - 시작 위치; direct - 방향 
+	
+	public int askBoard(int col, int row) {
+		return board[col][row];
+	} // kim
+	
+	public ArrayList<Point> getChild(Board board) {
+		return null;
+	} // seo
+	
+	public void calculateWeight() {
+		
+	} // 가로 세로 우대각선 좌대각선 전부 계산 시키는 함수
+	public int getScore(int start, int direct) {
+		return 0;
+	} // 6칸 짜리 값 업데이트 // start - 시작 위치; direct - 방향 
 	
 	/*
 	 * update board
